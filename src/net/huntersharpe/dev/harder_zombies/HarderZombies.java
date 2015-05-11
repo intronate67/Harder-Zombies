ec5926ddc961612fde0d4810ff191e459c36d651
Hunter Sharpe <huntersharpe@me.com>
2015-05-10 19:45:22 -0500

    Create HarderZombies.java

---
+++ src/net/huntersharpe/dev/harder_zombies/HarderZombies.java
@@ -0,0 +1,14 @@
+package net.huntersharpe.dev.harder_zombies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HarderZombies extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	public void onDisable(){
+		
+	}
+
+}
