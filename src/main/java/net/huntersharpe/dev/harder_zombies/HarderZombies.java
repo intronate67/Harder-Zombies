e7a39de4e875c46fa40e610ede9e61dad07495cc
BuildTools <unconfigured@null.spigotmc.org>
2015-05-11 19:12:25 -0500

    Added Methods

--- src/main/java/net/huntersharpe/dev/harder_zombies/HarderZombies.java
+++ src/main/java/net/huntersharpe/dev/harder_zombies/HarderZombies.java
@@ -1,7 +1,20 @@
 package main.java.net.huntersharpe.dev.harder_zombies;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Hunter Sharpe on 5/10/15.
  */
-public class HarderZombies {
+public class HarderZombies extends JavaPlugin {
+
+    public void onEnable() {
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new ZombieListener(), this);
+    }
+
+    public void onDisable() {
+
+    }
 }
--- src/main/java/net/huntersharpe/dev/harder_zombies/ZombieListener.java
+++ src/main/java/net/huntersharpe/dev/harder_zombies/ZombieListener.java
@@ -1,7 +1,19 @@
 package main.java.net.huntersharpe.dev.harder_zombies;
 
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
 /**
  * Created by Hunter Sharpe on 5/10/15.
  */
-public class ZombieListener {
+public class ZombieListener implements Listener {
+
+    public void onEntityDamage(EntityDamageByEntityEvent e){
+        if(!e.getEntityType().equals(EntityType.ZOMBIE)) return;
+        if(!e.getDamager().equals(EntityType.PLAYER)) return;
+
+    }
+
 }
